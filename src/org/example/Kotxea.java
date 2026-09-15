e6d912c1ba7763526a325f477695df05a6180932
Aritz Alonso <aalonso.25@izarraitz.eus>
2026-09-15 12:22:40 +0200

    konstante bat gehitu

--- src/org/example/Kotxea.java
+++ src/org/example/Kotxea.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Kotxea extends Ibilgailua{
+    final static int GURPIL_KOPURUA = 4;
     public Kotxea(String modeloa, String marka, int urtea) {
         super(modeloa, marka, urtea);
 }
